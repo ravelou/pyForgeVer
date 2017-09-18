7a0e90a78810e6a875f433324dec9e5f728cf947
ravelou <ravelou@gmail.com>
2017-09-18 22:12:41 +0200

    delete global variables - replaced by class attributes

--- pyforgever.py
+++ pyforgever.py
@@ -1,18 +1,24 @@
 # -*- coding: utf-8 -*-
 """Application to run on server so that it forges forever"""
-import io
+
+import json
+import os
+import subprocess
+
+from arky import api
+
 try:
     to_unicode = unicode
 except NameError:
     to_unicode = str
-import json
-from arky import api
 
-API_PORT = 4002  # 4001 for mainnet, 4002 for devnet
-if API_PORT == 4001:
-    SERVER_NETWORK = "ark"
-elif API_PORT == 4002:
-    SERVER_NETWORK = "dark"
+# API_PORT = 4002  # 4001 for mainnet, 4002 for devnet
+# if API_PORT == 4001:
+#     SERVER_NETWORK = "ark"
+#     FILE_SUFFIX = 'mainnet'
+# elif API_PORT == 4002:
+#     SERVER_NETWORK = "dark"
+#     FILE_SUFFIX = 'devnet'
 
 
 class Server():
@@ -26,10 +32,31 @@ class Server():
     return: hey\n
     rtype: so\n
 """
+    networks = {
+        'ark':{
+                'port':4001,
+                'file_suffix':'mainnet'
+        },
+        'dark':{
+                'port':4002,
+                'file_suffix':'devnet'
+        }
+    }
 
-    global API_PORT, SERVER_NETWORK
+    network='dark'
+    port=4002
+    file_suffix='devnet'
 
-    def __init__(self, server_ip, server_name=None, server_port=API_PORT):
+    @classmethod
+    def get_network(cls, server_port):
+        """Initialize global variable for the class"""
+        for (name,config) in cls.networks.items():
+            if server_port == config['port']:
+                cls.port = config['port']
+                cls.file_suffix = config['file_suffix']
+
+
+    def __init__(self, server_ip, server_port, server_name=None, ):
         if not server_name:
             self.server_name = server_ip
         else:
@@ -38,8 +65,9 @@ class Server():
         self.server_port = server_port
         self.server_seed = 'http://' + \
             str(self.server_ip) + ':' + str(self.server_port)
+        self.__class__.get_network(server_port)
         try:
-            api.use(SERVER_NETWORK, custom_seed=self.server_seed)
+            api.use(self.__class__.network, custom_seed=self.server_seed)
             self.server_blockchain_height = api.Block.getBlockchainHeight()[
                 'height']
         except:
@@ -63,37 +91,66 @@ class Server():
             self.server_blockchain_height = api.Block.getBlockchainHeight()[
                 'height']
         else:
-            api.use(SERVER_NETWORK, custom_seed=self.server_seed)
+            api.use(self.__class__.network, custom_seed=self.server_seed)
             self.server_blockchain_height = api.Block.getBlockchainHeight()[
                 'height']
         return int(self.server_blockchain_height)
 
+    
     def check_other_server_height(self, other_server):
         """To check the height of another server on the same port
         rtype: int"""
         if isinstance(other_server, Server):
-            api.use(SERVER_NETWORK, custom_seed=other_server.server_seed)
+            api.use(self.__class__.network, custom_seed=other_server.server_seed)
             other_server.update_server_blockchain_height()
             return other_server
         return 0
 
-    def get_delegate_passphrase(self, config_file):
+    @staticmethod
+    def get_delegate_passphrase(config_file):
         """Delegate function to get the delegate passphrase
-        config_file: the path to the configuration file where you get your passphrase"""
+        config_file: the path to the configuration file where you get your passphrase
+        """
+
         r_config_file = open(config_file, "r")
         config_json = api.ArkyDict = json.loads(r_config_file.read())
         return config_json['forging']['secret'][0]
 
-    def change_config_file(self, config_file, passphrase=''):
+    
+    def change_config_file(self, config_file,
+                           new_config_file='new.config.devnet.json',
+                           passphrase=''):
         """Function to put the passphrase into the main config file. Usually config.mainnet.json
-        config_file: main config file
-        passphrase: the passphrase you want to write into config_file"""
+        config_file: main config file to be change
+        new_config_file: main config file changed with the new passphrase
+        passphrase: the passphrase you want to write into config_file
+        """
+
         r_config_file = open(config_file, "r")
-        config_json = api.ArkyDict = json.loads(r_config_file.read())
+        config_json = api.ArkyDict(json.loads(r_config_file.read()))
         config_json['forging']['secret'][0] = passphrase
-        with io.open('data.json', 'w', encoding='utf8') as outfile:
+        with open(new_config_file, 'w', encoding='utf8') as outfile:
             str_ = json.dumps(config_json,
-                      indent=2,
-                      separators=(',', ': '), ensure_ascii=False)
+                              indent=2,
+                              separators=(',', ': '), ensure_ascii=False)
             outfile.write(to_unicode(str_))
 
+    @classmethod
+    def restart_server_node(cls, config_filename, arknode_dir_path=\
+                            "{0}/ark-node".format(os.getenv('HOME'))):
+        """method to restart the node from the current server
+        config_filename:the name of the config file you want to use to start the node\
+                        usually config.[mainnet/devnet].json
+        arknode_dir_path: the path where your app.js, config json file are
+        """
+        try:
+            p = subprocess.run(["forever", "stop", "{0}/app.js".format(arknode_dir_path)],
+                               stdout=subprocess.PIPE, check=True)
+        except OSError:
+            print("The was an error stopping ark-node : {0}".format(p.stdout))
+            return OSError.__cause__
+        os.chdir(arknode_dir_path)
+        r = subprocess.run(['forever', 'start', 'app.js', '--config', config_filename,
+                            '--genesis', 'genesisBlock.{0}.json'.format(cls.file_suffix)],
+                           stdout=subprocess.PIPE)
+        print(r.stdout)
