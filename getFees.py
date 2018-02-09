b64b24216fdc94bc9c533a1803fe4484d0d2be0f
ravelou <ravelou@gmail.com>
2018-02-09 16:09:44 +0100

    fixes

--- getFees.py
+++ getFees.py
@@ -1,44 +1,94 @@
 # -*- coding: utf-8 -*-
-"""Application to run on server so that it forges forever"""
+
 from datetime import datetime, timedelta
 import pytz
+import pyprind
 
 import arky.rest as arkApi
 
+def getSelfTransaction(delegate='ravelou'):
+    selfTransactions = []
+    sum=0
+    arkApi.use('ark')       #API initialization
+
+    delegateAddress = arkApi.GET.api.delegates.get(username=delegate)['delegate']['address']
+    while True:
+        Transactions = arkApi.GET.api.transactions(recipientId=delegateAddress)
+        if Transactions["success"]:
+            receivedTransactions = Transactions['transactions']
+            break
+        
+    for elt in receivedTransactions:
+        if elt['senderId'] == delegateAddress and elt['type'] == 0:
+            selfTransactions.append(elt['amount']/100000000)
+    for elt in selfTransactions:
+        sum+=elt
+    print(sum)
+    
+##*/
+def containsAValueAfterDateTime(forgedBlocksRequested, beginDatetime):
+    """
+    to be used, the arky.rest API has to be initialized
+    forgedBlocksRequested : list containing the transactions asked from arky.rest API
+    beginDatetime : Datetime containing the time of the last transaction you want
+    """
+    try:
+        if arkApi.slots.getRealTime(forgedBlocksRequested[0]['timestamp']) >= beginDatetime:
+            return True
+        else: return False
+    except IndexError:
+        return True
+
 def getFeesBetweenDates(beginDatetime, endDatetime=datetime.now(pytz.UTC), delegate='ravelou'):
-
-    dateDebut = endDatetime - timedelta(hours=7)
-
+    """
+    beginDatetime : the Datetime from where you want to start the counting fees 
+    beginTimetime : not use
+    delegate : delegate name
+    """
     limitRecord = 100       #100 : max record you can ask to ark api
     sumFees = 0             #fees from transaction forging
     offset_rqst = 0         #index used to get more than limit_Record blocks from the ARK API
-    forgedBlocks = []       #forged blocks before now and after dateDebut
+    forgedBlocks = []       #forged blocks before now and after beginDatetime
 
     arkApi.use('ark')       #API initialization
 
     delegatePublicKey = arkApi.GET.api.delegates.get(username=delegate)['delegate']['publicKey']
 
-    while True:
-        while True:
+    forgedBlocksRequested = []
+    while containsAValueAfterDateTime(forgedBlocksRequested,beginDatetime):
+           
+        blocksRequest = {}
+        while not blocksRequest.get("success", False):
             blocksRequest = arkApi.GET.api.blocks(limit=limitRecord, generatorPublicKey=delegatePublicKey, offset=offset_rqst)
-            if blocksRequest['success']:
-                forgedBlocksRequested = blocksRequest['blocks']
-                break
-        if arkApi.slots.getRealTime(forgedBlocksRequested[limitRecord-1]['timestamp']) < dateDebut:
+        
+        forgedBlocksRequested = blocksRequest['blocks']
+        if arkApi.slots.getRealTime(forgedBlocksRequested[-1]['timestamp']) < beginDatetime:
             for i, elt in enumerate(forgedBlocksRequested):
-                if arkApi.slots.getRealTime(elt['timestamp']) >= dateDebut:
-                    forgedBlocks.append(elt)
-            break
+                if arkApi.slots.getRealTime(elt['timestamp']) >= beginDatetime:
+                    forgedBlocks.append(elt)    
         else:
             forgedBlocks += forgedBlocksRequested
-            offset_rqst += limitRecord + 1
+        offset_rqst += limitRecord + 1
 
     for i, elt in enumerate(forgedBlocks):
-        sumFees += elt['totalFee']
-        #print("{0} : {1} ark".format(arkApi.slots.getRealTime(elt['timestamp']), elt['totalFee']/100000000))
+        sumFees += elt['totalFee']#elt['reward']
     return sumFees
 
 
-fees = getFeesBetweenDates(datetime.now(pytz.UTC) - timedelta(hours=7))
+"""
+    =============
+    |MainProgram|
+    =============
+"""
+dateInput = '04/02/2018'#input('Date de début (jj/mm/aaaa) : ')
+timeInput = '22:09'#input('Heure de début (hh:mm) : ')
 
-print("Nombre d'arks forgés en frais : {0}".format(fees/100000000))
+dateSplit = dateInput.split("/")
+timeSplit = timeInput.split(":")
+
+beginTime = datetime(int(dateSplit[2]),int(dateSplit[1]),int(dateSplit[0]),int(timeSplit[0]),int(timeSplit[1]),0,0,tzinfo=pytz.UTC)
+
+fees = getFeesBetweenDates(beginTime, delegate='ravelou')
+
+print("Nombre d'arks forgés en frais entre le {0} et le {1} : {2}".format(beginTime, datetime.now(pytz.UTC),fees/100000000))
+#getSelfTransaction()
